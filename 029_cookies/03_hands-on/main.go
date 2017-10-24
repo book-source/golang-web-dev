28978afd4089f8fb6397cd07ea67a89f3a4fdca1
Yongqiang Qu <netqyq@gmail.com>
2017-10-24 10:13:54 +0800

    cookie

--- 029_cookies/03_hands-on/main.go
+++ 029_cookies/03_hands-on/main.go
@@ -21,23 +21,23 @@ func count(w http.ResponseWriter, r *http.Request) {
 	//
 	// get current count var in cookie
 	// cur count is not set
-	access, err := r.Cookie("access")
+	count, err := r.Cookie("count")
 	if err != nil {
 		log.Println("count is not set", err)
-		access = &http.Cookie{Name: "access", Value: "0"}
+		count = &http.Cookie{Name: "count", Value: "0"}
 		// 加上这句以后则cookie设置不成功！！！！
 		// fmt.Fprintln(w, "new value is set")
 	}
 	//
 	//
 	// set count = count+1 in new cookie
-	accessCountValue, err := strconv.Atoi(access.Value)
+	countCountValue, err := strconv.Atoi(count.Value)
 	if err != nil {
 		log.Fatalln("conv to int err")
 	}
-	accessCountValue++
-	access.Value = strconv.Itoa(accessCountValue)
-	http.SetCookie(w, access)
-	fmt.Fprintln(w, "new value is set", access.Value)
-	// io.WriteString(w, access.Value)
+	countCountValue++
+	count.Value = strconv.Itoa(countCountValue)
+	http.SetCookie(w, count)
+	fmt.Fprintln(w, "new value is set", count.Value)
+	// io.WriteString(w, count.Value)
 }
